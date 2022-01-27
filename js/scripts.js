285453c1b1b5682fec2619d788eac0ad1391687d
Yazmin Perez <zoeperez333@gmail.com>
2022-01-27 11:33:39 -0800

    add js function, const videoGame, cityWork, mythCreature, codeExp, islandItem

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $("form#selector").submit(function(event) {
+    const videoGame = $("input#videoGame".val();
+    const cityWork = $("input#cityWork").val();
+    const mythCreature = $("input#mythCreature").val();
+    const codeExp = $("input#codeExp").val();
+    const islandItem = $("input#islandItem").val();
+
+  
+
+  }
+}
