ef1ca078575d379a3b66af618811cb15cadf2cf2
Yazmin Perez <zoeperez333@gmail.com>
2022-01-27 12:30:07 -0800

    add branching in js

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,37 @@
 $(document).ready(function() {
   $("form#selector").submit(function(event) {
-    const videoGame = $("input#videoGame".val();
+    const fullName = $("input#fullName").val();
+    const videoGame = $("input#videoGame").val();
     const cityWork = $("input#cityWork").val();
     const mythCreature = $("input#mythCreature").val();
     const codeExp = $("input#codeExp").val();
     const islandItem = $("input#islandItem").val();
 
-  
+    $("#fullName").text(fullName);
 
-  }
-}
+  
+    if (videoGame === "videoGame1" && cityWork == "cityWork1" && mythCreature === "mythCreature1" && codeExp === "codeExp1" && islandItem === "islandItem1") {
+    $(".result").show();
+    $("#python").show();
+    $("#javaScript").hide();
+    $("#ruby").hide();
+    $("#form#selector").hide();
+    } else if 
+      (videoGame === "yes" && cityWork === "newYork" && mythCreature === "unicorn" && codeExp === "some" && islandItem === "book") {
+        $(".result").show();
+        $("#python").hide();
+        $("#javaScript").show();
+        $("#ruby").hide();
+        $("#form#selector").hide();
+    } else { 
+    (videoGame === "no" && cityWork == "remote" && mythCreature === "neither" && codeExp === "newbie" && islandItem === "dvd")
+      $(".result").show();
+      $("#python").hide();
+      $("#javaScript").hide();
+      $("#ruby").show();
+      $("#form#selector").hide();
+
+      event.preventDefault();
+    }
+  });
+});
