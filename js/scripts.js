4d0e80eb175606f8ddcaf241e932d4d68da075f5
Yazmin Perez <zoeperez333@gmail.com>
2022-02-03 12:16:22 -0800

    fix scripts.js indentation errors, remove empty lines at the bottom of each branching, condense hide() results into one location, add reset button to html & scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,84 +1,53 @@
 $(document).ready(function() {
   $("form#selector").submit(function(event) {
     event.preventDefault();
-
+    
     const fullName1Input = $("input#fullName").val();
-        const videoGame = $("input:radio[name=videoGame]:checked").val();
-        const cityWork = $("input:radio[name=cityWork]:checked").val();
-        const mythCreature = $("input:radio[name=mythCreature]:checked").val();
-        const codeExp = $("input:radio[name=codeExp]:checked").val();
-        const islandItem = $("input:radio[name=islandItem]:checked").val();
-
-        $(".fullName").text(fullName1Input);
-        $(".result").show();
-        $("form#selector").hide();
-
-     if (videoGame === "yes" && cityWork == "seattle" && mythCreature === "dragon" && codeExp === "tons" && islandItem === "gameBoy") {
+    const videoGame = $("input:radio[name=videoGame]:checked").val();
+    const cityWork = $("input:radio[name=cityWork]:checked").val();
+    const mythCreature = $("input:radio[name=mythCreature]:checked").val();
+    const codeExp = $("input:radio[name=codeExp]:checked").val();
+    const islandItem = $("input:radio[name=islandItem]:checked").val();
+    
+    $(".fullName").text(fullName1Input);
+    $(".result").show();
+    $("#reset").show();
+    $("form#selector").hide();
+    $("#python").hide();
+    $("#javaScript").hide();
+    $("#ruby").hide();
+    $("#cSharp").hide();
+    $("#java").hide();
+    $("#swift").hide();
+    if (videoGame === "yes" && cityWork == "seattle" && mythCreature === "dragon" && codeExp === "tons" && islandItem === "gameBoy") {
       $("#python").show();
-      $("#javaScript").hide();
-      $("#ruby").hide();
-      $("#cSharp").hide();
-      $("#java").hide();
-      $("#swift").hide();
-      
-  
-    } else if (
-      videoGame === "yes" && cityWork === "newYork" && mythCreature === "unicorn" && codeExp === "some" && islandItem === "book"
-      ) {
-      $("#python").hide();
+    } else if (videoGame === "yes" && cityWork === "newYork" && mythCreature === "unicorn" && codeExp === "some" && islandItem === "book") {
       $("#javaScript").show();
-      $("#ruby").hide();
-      $("#cSharp").hide();
-      $("#java").hide();
-      $("#swift").hide();
-      
- 
     } else if (
-      videoGame === "no" && cityWork == "remote" && mythCreature === "neither" && codeExp === "newbie" && islandItem === "dvd"
-      ) {
-      $("#python").hide();
-      $("#javaScript").hide();
+      videoGame === "no" && cityWork == "remote" && mythCreature === "neither" && codeExp === "newbie" && islandItem === "dvd") {
       $("#ruby").show();
-      $("#cSharp").hide();
-      $("#java").hide();
-      $("#swift").hide();
-      
-
     } else if (
-      videoGame === "no" && cityWork == "seattle" && mythCreature === "unicorn" && codeExp === "some" && islandItem === "gameBoy"
-      ) {
-      $("#python").hide();
-      $("#javaScript").hide();
-      $("#ruby").hide();
+      videoGame === "no" && cityWork == "seattle" && mythCreature === "unicorn" && codeExp === "some" && islandItem === "gameBoy") {
       $("#cSharp").show();
-      $("#java").hide();
-      $("#swift").hide();
-      
-
     } else if (
-      videoGame === "yes" && cityWork == "remote" && mythCreature === "neither" && codeExp === "tons" && islandItem === "dvd"
-      ) {
-      $("#python").hide();
-      $("#javaScript").hide();
-      $("#ruby").hide();
-      $("#cSharp").hide();
+      videoGame === "yes" && cityWork == "remote" && mythCreature === "neither" && codeExp === "tons" && islandItem === "dvd") {
       $("#java").show();
-      $("#swift").hide();
-      
-
     } else {
-      $("#python").hide();
-      $("#javaScript").hide();
-      $("#ruby").hide();
-      $("#cSharp").hide();
-      $("#java").hide();
       $("#swift").show();
-      
     }
-  });
-
-  $(document).on("change",".switch",function() {
-    $("body").toggleClass("dark");
-
+    $("#reset").click(function() {
+      $("#selector").toggle().show();
+      $("#reset").toggle().hide();
+      $("#python").toggle().hide();
+      $("#javaScript").toggle().hide();
+      $("#ruby").toggle().hide();
+      $("#cSharp").toggle().hide();
+      $("#java").toggle().hide();
+      $("#swift").toggle().hide();
+      document.getElementById("selector").reset();
     });
   });
+  $(document).on("change",".switch",function() {
+    $("body").toggleClass("dark");
+  });
+});
